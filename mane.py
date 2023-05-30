52ebc4ab3603fe31d3254e876350940dbb7aa699
Yevhen Lobanov <yevhen.23.10@gmail.com>
2023-05-30 16:13:32 +0300

    002 New test_function created

--- mane.py
+++ mane.py
@@ -4,3 +4,11 @@ def mane_function(a, b):
     square = a**b
     return addition, subtraction, square
 result_addition, result_subtraction, result_square = mane_function(10, 2)
+
+
+
+def test_function(a, b):
+    multiplication = a*b
+    division = a/b
+    return multiplication, division
+    
