4bffd4b6d9a5d55229ae68726150392a1d4779fd
Yevhen Lobanov <yevhen.23.10@gmail.com>
2023-05-30 16:38:53 +0300

    010 added result output

--- py_charm.py
+++ py_charm.py
@@ -4,4 +4,7 @@ def mane_function(a, b):
     cub = (a**b)
     return plus, minus, cub
 result_plus, result_minus, result_cub = mane_function(10, 3)
+print("Результат з додаванням:", result_plus)
+print("Результат з відніманням:", result_minus)
+print("Зведення в куб:", result_cub)
 
