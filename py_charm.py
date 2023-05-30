fbf673f80ad9935b23602c09df5047b56ee7c353
Yevhen Lobanov <yevhen.23.10@gmail.com>
2023-05-30 16:41:42 +0300

    006 definition of function arguments and added result output

--- py_charm.py
+++ py_charm.py
@@ -2,3 +2,6 @@ def test_function(a, b):
     multiplication = a*b
     division = a/b
     return multiplication, division
+result_multiplication, result_division = test_function(10, 2)
+print("Результат множення:", result_multiplication)
+print("Результат ділення:", result_division)
