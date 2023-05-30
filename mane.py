f66578c35288a96f26b065606503f02241123e39
Yevhen Lobanov <yevhen.23.10@gmail.com>
2023-05-30 16:10:28 +0300

    004 definition of function arguments

--- mane.py
+++ mane.py
@@ -3,4 +3,4 @@ def mane_function(a, b):
     subtraction = a - b
     square = a**b
     return addition, subtraction, square
-    
+result_addition, result_subtraction, result_square = mane_function(10, 2)
