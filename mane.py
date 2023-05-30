8c83d46b58c734888b10e93dc3dd983a989b5cb5
Yevhen Lobanov <yevhen.23.10@gmail.com>
2023-05-30 16:14:14 +0300

    003 definition of function arguments and added result output

--- mane.py
+++ mane.py
@@ -11,4 +11,8 @@ def test_function(a, b):
     multiplication = a*b
     division = a/b
     return multiplication, division
-    
+
+
+result_multiplication, result_division = test_function(10, 2)
+print("Результат множення:", result_multiplication)
+print("Результат ділення:", result_division)
