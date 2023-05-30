28d2848dad3dd912a2582b576782a3d9336e2bde
Yevhen Lobanov <yevhen.23.10@gmail.com>
2023-05-30 16:11:33 +0300

    005 added result output

--- mane.py
+++ mane.py
@@ -4,3 +4,6 @@ def mane_function(a, b):
     square = a**b
     return addition, subtraction, square
 result_addition, result_subtraction, result_square = mane_function(10, 2)
+print("Результат додавання:", result_addition)
+print("Результат віднімання:", result_subtraction)
+print("Зведення в ступінь:", result_square)
