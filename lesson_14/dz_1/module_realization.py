c11ca27022a5a38befc634668e6f21ac5ce04354
Yevhen Lobanov <yevhen.23.10@gmail.com>
2023-06-16 10:01:59 +0300

    lesson_14

--- lesson_14/dz_1/module_realization.py
+++ lesson_14/dz_1/module_realization.py
@@ -1,8 +1,8 @@
 from class_Tree import Tree
 import random
 dat = []
-for _ in range(50):
-    dat.append(random.randint(0, 100))
+for _ in range(25):
+    dat.append(random.randint(0, 50))
 #Створюємо єкземпляр класа Tree в якому кореневим вузлом буде перший елемент сгенерованого нами списку
 tree = Tree(dat[0])
 
