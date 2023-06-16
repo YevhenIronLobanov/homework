b65daec420dfab4f85acd9e59fd4a91a2bcc2ec9
Yevhen Lobanov <yevhen.23.10@gmail.com>
2023-06-16 10:01:00 +0300

    lesson_14

--- lesson_14/dz_2/module_realization.py
+++ lesson_14/dz_2/module_realization.py
@@ -2,17 +2,17 @@ from class_Tree import Tree
 import random
 
 dat = []
-for _ in range(50):
-    dat.append(random.randint(0, 100))
+for _ in range(25):
+    dat.append(random.randint(1, 50))
 
 # Створюємо екземпляр класу Tree, в якому кореневим вузлом буде перший елемент сгенерованого нами списку
 tree = Tree(dat[0])
 
 tree.add_elements(dat[1:])
 tree.display()
-b = Tree.find_min(tree)
-c = Tree.find_max(tree)
-print('Мінімальне значення бінарного дерева:', b)
-print('Максимальне значення бінарного дерева:', c)
+min_element = tree.find_min()
+max_element = tree.find_max()
+print('Мінімальне значення бінарного дерева:', min_element)
+print('Максимальне значення бінарного дерева:', max_element)
 
 
